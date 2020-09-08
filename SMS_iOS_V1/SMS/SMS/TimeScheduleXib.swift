ee85a048f1807dec57e6a5ab448e8a251406abc1
이현욱 <hjhsha827@gmail.com>
2020-09-08 23:34:05 +0900

    [Add] TimeScheduleXib.xib and TimeScheduleXib.swift

---
+++ SMS_iOS_V1/SMS/SMS/TimeScheduleXib.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class TimecScheduleXib : UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        guard let xibName = NSStringFromClass(self.classForCoder).components(separatedBy: ".").last else { return }
+        let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
