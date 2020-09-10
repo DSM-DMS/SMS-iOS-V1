acd969160c3fb369c0ee8e6bf119e89cd9c18e0c
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-10 15:54:03 +0900

    [Add] OutGoingViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/OutGoingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OutGoingViewController.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/10.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class OutGoingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
