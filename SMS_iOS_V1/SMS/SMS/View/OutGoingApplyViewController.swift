8f94ad55dde5983f84dc02c9b5b1804dc30ac6ed
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-11 18:30:44 +0900

    [Modify] OutGoingViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoingApplyViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoingApplyViewController.swift
@@ -8,9 +8,27 @@
 
 import UIKit
 class OutGoingApplyViewController: UIViewController {
+    
+    @IBOutlet weak var topBar: UIView!
+    @IBOutlet weak var applyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUi()
         
+    }
+    
+    func setupUi() {
+        topBar.layer.shadowColor = UIColor.lightGray.cgColor
+        topBar.layer.shadowOpacity = 0.7
+        topBar.layer.shadowOffset = CGSize(width: 0, height: 2)
+        topBar.layer.shadowRadius = 2
+        
+        applyButton.layer.cornerRadius = 3
+        applyButton.layer.shadowColor = UIColor.lightGray.cgColor
+        applyButton.layer.shadowOpacity = 0.7
+        applyButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        applyButton.layer.shadowRadius = 2
     }
     
 }
