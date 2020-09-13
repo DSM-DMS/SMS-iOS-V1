bcda73706f18caa8fe7454c576de77a9453d5bcd
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-13 21:25:42 +0900

    [Add] OutGoingCompletedViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/OutGoingCompletedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OutGoingCompletedViewController.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/13.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class OutGoingCompletedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
