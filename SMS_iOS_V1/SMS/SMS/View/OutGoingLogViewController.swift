8f4ae4eb3185e729d0e79c7511a40d3d9a7dd3c7
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-13 22:57:25 +0900

    [Add] OutGoingLogViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/OutGoingLogViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OutGoingLogViewController.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/13.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class OutGoingLogViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
