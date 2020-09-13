6445b605785fe95c8e27f3e29b6f4524c9cd649e
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-13 23:25:14 +0900

    [Modify] OutGoingNoticeViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoingNoticeViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoingNoticeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class OutGoingViewController: UIViewController {
+class OutGoingNoticeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
