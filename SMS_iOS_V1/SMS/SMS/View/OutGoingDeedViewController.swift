cf2bc9fd3bf6fd47c5050624c50013d7a189fe56
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-14 21:43:39 +0900

    [Add] OutGoingDeedViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/OutGoingDeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OutGoingDeedViewController.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/14.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class OutGoingDeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
