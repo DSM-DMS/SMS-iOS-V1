c74cdbdbe92cfe996c4ee78e58aed35c5d1e2348
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-15 19:47:12 +0900

    [Add] MypageViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/MypageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MypageViewController.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/15.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class MypageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
