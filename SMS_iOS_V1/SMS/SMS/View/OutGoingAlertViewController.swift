79febc6e31dd460a9597bec33e44d901dd6ab575
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-15 13:55:45 +0900

    [Add] func setupUI

--- SMS_iOS_V1/SMS/SMS/View/OutGoingAlertViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoingAlertViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class OutGoingAlertViewController: UIViewController {
     
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         
     }
-    
+    func setupUI() {
+        
+        backgroundView.layer.cornerRadius = 10
+        backgroundView.layer.shadowColor = UIColor.lightGray.cgColor
+        backgroundView.layer.shadowOpacity = 0.7
+        backgroundView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        backgroundView.layer.shadowRadius = 2
+    }
 }
