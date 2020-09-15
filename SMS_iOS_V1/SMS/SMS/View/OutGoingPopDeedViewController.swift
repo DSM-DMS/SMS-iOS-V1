6e336684135f5e2ab4d4b5efe94eb6775d65ea1b
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-15 14:38:43 +0900

    [Add] OutGoingPopDeedViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/OutGoingPopDeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OutGoingPopDeedViewController.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/15.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class OutGoingPopDeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
