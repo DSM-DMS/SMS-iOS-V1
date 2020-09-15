cf9e6165bb9b36dfbb8fcac4b620928b74cdc0cd
이현욱 <hjhsha827@gmail.com>
2020-09-15 23:38:57 +0900

    [Modify] CalendarCollectionViewCell

--- SMS_iOS_V1/SMS/SMS/CalendarCollectionViewCell.swift
+++ SMS_iOS_V1/SMS/SMS/CalendarCollectionViewCell.swift
@@ -11,8 +11,62 @@ enum SelectionType : Int {
 }
 
 class CalendarCollectionViewCell: FSCalendarCell {
+
+    weak var eventLayer: CAShapeLayer!
+
+    var selectionType: SelectionType = .none {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    required init!(coder aDecoder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        let selectionLayer = CAShapeLayer()
+        selectionLayer.fillColor = UIColor.init(red: 246/255, green: 246/255, blue: 246/255, alpha: 1).cgColor
+        selectionLayer.actions = ["hidden": NSNull()]
+        self.titleLabel.layer.insertSublayer(<#T##layer: CALayer##CALayer#>, below: <#T##CALayer?#>)
+        
+        self.contentView.layer.insertSublayer(selectionLayer, below: self.titleLabel!.layer)
+        self.eventLayer = selectionLayer
+
+        self.shapeLayer.zPosition = 1
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.eventLayer.frame = self.contentView.bounds
+
+        if selectionType == .middle {
+            self.eventLayer.path = UIBezierPath(rect: self.eventLayer.bounds).cgPath
+        } else if selectionType == .leftBorder {
+            self.eventLayer.path = UIBezierPath(roundedRect: self.eventLayer.bounds, byRoundingCorners: [.topLeft, .bottomLeft], cornerRadii: CGSize(width: self.eventLayer.frame.width / 2, height: self.eventLayer.frame.width / 2)).cgPath
+        } else if selectionType == .rightBorder {
+            self.eventLayer.path = UIBezierPath(roundedRect: self.eventLayer.bounds, byRoundingCorners: [.topRight, .bottomRight], cornerRadii: CGSize(width: self.eventLayer.frame.width / 2, height: self.eventLayer.frame.width / 2)).cgPath
+        } else if selectionType == .single {
+            let diameter: CGFloat = min(self.eventLayer.frame.height, self.eventLayer.frame.width)
+            self.eventLayer.path = UIBezierPath(ovalIn: CGRect(x: self.contentView.frame.width / 2 - diameter / 2, y: self.contentView.frame.height / 2 - diameter / 2, width: diameter, height: diameter)).cgPath
+        }
+    }
     
+//    override func configureAppearance() {
+//        super.configureAppearance()
+//        self.titleLabel.textColor = UIColor.black
+//    }
+}
+
+class DIYCalendarCell: FSCalendarCell {
+    
+    lazy var UInt = shapeLayer.zPosition
+    weak var view: UIView!
+    weak var eventLayer: CAShapeLayer!
     weak var selectionLayer: CAShapeLayer!
+    
     
     var selectionType: SelectionType = .none {
         didSet {
@@ -27,34 +81,48 @@ class CalendarCollectionViewCell: FSCalendarCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        let selectionLayer = CAShapeLayer()
-        selectionLayer.fillColor = UIColor.purple.cgColor
-        selectionLayer.actions = ["hidden": NSNull()]
-        self.contentView.layer.insertSublayer(selectionLayer, below: self.titleLabel!.layer)
-        self.selectionLayer = selectionLayer
+        let view = UIView()
+        self.contentView.insertSubview(view, at: 0)
+        self.view = view
         
-        self.shapeLayer.isHidden = true
+        let eventLayerSet = CAShapeLayer()
+        let selectionLayerSet = CAShapeLayer()
+        
+        eventLayerSet.actions = ["hidden": NSNull()]
+        self.contentView.layer.insertSublayer(eventLayerSet, below: self.titleLabel!.layer)
+        self.contentView.layer.insertSublayer(eventLayerSet, at: UInt32(UInt))
+        self.eventLayer = eventLayerSet
+        
+        selectionLayerSet.fillColor = UIColor.init(displayP3Red: 83/255, green: 35/255, blue: 178/255, alpha: 1).cgColor
+        selectionLayerSet.actions = ["hidden": NSNull()]
+        self.contentView.layer.insertSublayer(selectionLayerSet, below: self.titleLabel!.layer)
+        self.selectionLayer = selectionLayerSet
+        
+        self.shapeLayer.zPosition = CGFloat(integerLiteral: 1)
+        
+        
+        
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        self.view.frame = self.contentView.bounds
         self.backgroundView?.frame = self.bounds.insetBy(dx: 1, dy: 1)
-        self.selectionLayer.frame = self.contentView.bounds
+        self.eventLayer.frame = self.contentView.bounds
         
         if selectionType == .middle {
-            self.selectionLayer.path = UIBezierPath(rect: self.selectionLayer.bounds).cgPath
+            self.eventLayer.path = UIBezierPath(rect: self.eventLayer.bounds).cgPath
         }
         else if selectionType == .leftBorder {
-            let corner : UIRectCorner = [.topLeft, .bottomLeft]
-            self.selectionLayer.path = UIBezierPath(roundedRect: self.selectionLayer.bounds, byRoundingCorners: corner, cornerRadii: CGSize(width: self.selectionLayer.frame.width / 2, height: self.selectionLayer.frame.width / 2)).cgPath
+            self.eventLayer.path = UIBezierPath(roundedRect: self.eventLayer.bounds, byRoundingCorners: [.topLeft, .bottomLeft], cornerRadii: CGSize(width: self.eventLayer.frame.width / 2, height: self.eventLayer.frame.width / 2)).cgPath
         }
         else if selectionType == .rightBorder {
-            let corner : UIRectCorner = [.topRight, .bottomRight]
-            self.selectionLayer.path = UIBezierPath(roundedRect: self.selectionLayer.bounds, byRoundingCorners: corner, cornerRadii: CGSize(width: self.selectionLayer.frame.width / 2, height: self.selectionLayer.frame.width / 2)).cgPath
+            self.eventLayer.path = UIBezierPath(roundedRect: self.eventLayer.bounds, byRoundingCorners: [.topRight, .bottomRight], cornerRadii: CGSize(width: self.eventLayer.frame.width / 2, height: self.eventLayer.frame.width / 2)).cgPath
         }
         else if selectionType == .single {
-            let diameter: CGFloat = min(self.selectionLayer.frame.height, self.selectionLayer.frame.width)
-            self.selectionLayer.path = UIBezierPath(ovalIn: CGRect(x: self.contentView.frame.width / 2 - diameter / 2, y: self.contentView.frame.height / 2 - diameter / 2, width: diameter, height: diameter)).cgPath
+            let diameter: CGFloat = min(self.eventLayer.frame.height, self.eventLayer.frame.width)
+            self.eventLayer.path = UIBezierPath(ovalIn: CGRect(x: self.contentView.frame.width / 2 - diameter / 2, y: self.contentView.frame.height / 2 - diameter / 2, width: diameter, height: diameter)).cgPath
         }
     }
     
@@ -64,6 +132,7 @@ class CalendarCollectionViewCell: FSCalendarCell {
         if self.isPlaceholder {
             self.eventIndicator.isHidden = true
             self.titleLabel.textColor = UIColor.lightGray
+            self.titleLabel.layer.zPosition = 0
         }
     }
 }
