71f51394b9a8a3d9fe498370601bf20ee95d1e5a
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-16 09:05:42 +0900

    [Add] MypageChangePWAlertViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/MypageChangePWAlertViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MypageChangePWAlert.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/16.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class MypageChangePWAlertViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
