f6773f28f92a6f622761afb6db36744599c9f04d
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-16 20:02:34 +0900

    [ReFactor] LoginUI

--- SMS_iOS_V1/SMS/SMS/View/LoginViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/LoginViewController.swift
@@ -10,31 +10,13 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var LoginButton: LoginButtonUIExtention!
     @IBOutlet weak var IDTextField: UITextField!
     @IBOutlet weak var PWTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-    }
-    
-    @IBAction func autoLogin(_ sender: UIButton) {
-       
-        
         
     }
-    
-    
-    func setupUI() {
-        LoginButton.layer.cornerRadius = 3
-        LoginButton.layer.shadowColor = UIColor.lightGray.cgColor
-        LoginButton.layer.shadowOpacity = 0.7
-        LoginButton.layer.shadowOffset = CGSize(width: 0, height: 2)
-        LoginButton.layer.shadowRadius = 2
-    
-    }
-    
-    
     
 }
