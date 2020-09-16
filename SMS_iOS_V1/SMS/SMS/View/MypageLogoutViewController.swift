17831b6154aadb58d0ecab46845f39396d267e50
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-16 09:23:34 +0900

    [Add] MypageLogoutViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/MypageLogoutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MypageLogoutViewController.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/16.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class MypageLogoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
