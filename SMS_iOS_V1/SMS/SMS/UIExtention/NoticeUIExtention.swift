4913ee3560223a5cd3dd38958f0820b942ccadb0
DohyunKIm <rlaehgus1229@gmail.com>
2020-09-16 19:30:44 +0900

    [Add] Swift Files

---
+++ SMS_iOS_V1/SMS/SMS/UIExtention/NoticeUIExtention.swift
@@ -0,0 +1,9 @@
+//
+//  NoticeUIExtention.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/16.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
---
+++ SMS_iOS_V1/SMS/SMS/UIExtention/OutGoingUIExtention.swift
@@ -0,0 +1,9 @@
+//
+//  OutGoingUIExtention.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/09/16.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
