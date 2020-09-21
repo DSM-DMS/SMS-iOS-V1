850e4fa48c155a13ad6cc744374d63834ba8e0c7
이현욱 <hjhsha827@gmail.com>
2020-09-21 15:20:17 +0900

    [Add] ScheduleCell

---
+++ SMS_iOS_V1/SMS/SMS/ScheduleCell.swift
@@ -0,0 +1,23 @@
+//
+//  ScheduleCell.swift
+//  
+//
+//  Created by 이현욱 on 2020/09/21.
+//
+
+import UIKit
+
+class ScheduleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
