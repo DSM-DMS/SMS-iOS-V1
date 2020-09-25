5707b4e7e6fb85b265982c0eaa76a1e5ff90cc1e
이현욱 <hjhsha827@gmail.com>
2020-09-25 23:53:45 +0900

    [Modify] TabbarView

--- SMS_iOS_V1/SMS/SMS/TabbarView.swift
+++ SMS_iOS_V1/SMS/SMS/TabbarView.swift
@@ -14,10 +14,12 @@ protocol CustomMenuBarDelegate: class {
 
 class CustomMenuBar: UIView {
     
+    let storyboardNames: [Int:String] = [0:"Login",1:"OutGoing",2:"Notice",3:"MyPage"]
+    let viewcontrollerNames: [Int:String] = [0:"LoginViewController",1:"OutGoingViewController",2:"NoticeViewController",3:"MypageViewController"]
+    
     weak var delegate: CustomMenuBarDelegate?
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .white
         setupCustomTabBar()
     }
     
@@ -37,7 +39,7 @@ class CustomMenuBar: UIView {
     var indicatorView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .black
+        view.backgroundColor = .init(red: 83/255, green: 35/255, blue: 178/255, alpha: 1)
         return view
     }()
     //MARK: Properties
@@ -48,7 +50,7 @@ class CustomMenuBar: UIView {
         customTabBarCollectionView.delegate = self
         customTabBarCollectionView.dataSource = self
         customTabBarCollectionView.showsHorizontalScrollIndicator = false
-        customTabBarCollectionView.register(UINib(nibName: TabbarCell.reusableIdentifier, bundle: nil), forCellWithReuseIdentifier: TabbarCell.reusableIdentifier)
+        customTabBarCollectionView.register(UINib(nibName: CustomCell.reusableIdentifier, bundle: nil), forCellWithReuseIdentifier: CustomCell.reusableIdentifier)
         customTabBarCollectionView.isScrollEnabled = false
         
         let indexPath = IndexPath(item: 0, section: 0)
@@ -61,12 +63,18 @@ class CustomMenuBar: UIView {
         customTabBarCollectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         customTabBarCollectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         customTabBarCollectionView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        customTabBarCollectionView.heightAnchor.constraint(equalToConstant: 55).isActive = true
-        
+        customTabBarCollectionView.heightAnchor.constraint(equalToConstant: 62).isActive = true
+        customTabBarCollectionView.layer.masksToBounds = false
+        customTabBarCollectionView.layer.cornerRadius = 20
+        customTabBarCollectionView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        customTabBarCollectionView.addShadow(offset: CGSize(width: 0, height: -2.5),
+                                     color: .gray,
+                                     radius: CGFloat(2),
+                                     opacity: 0.5)
         self.addSubview(indicatorView)
-        indicatorViewWidthConstraint = indicatorView.widthAnchor.constraint(equalToConstant: self.frame.width / 4)
+        indicatorViewWidthConstraint = indicatorView.widthAnchor.constraint(equalToConstant: self.frame.width / 4 + 30)
         indicatorViewWidthConstraint.isActive = true
-        indicatorView.heightAnchor.constraint(equalToConstant: 5).isActive = true
+        indicatorView.heightAnchor.constraint(equalToConstant: 2).isActive = true
         indicatorViewLeadingConstraint = indicatorView.leadingAnchor.constraint(equalTo: self.leadingAnchor)
         indicatorViewLeadingConstraint.isActive = true
         indicatorView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
@@ -78,7 +86,8 @@ class CustomMenuBar: UIView {
 extension CustomMenuBar: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TabbarCell.reusableIdentifier, for: indexPath) as! TabbarCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCell.reusableIdentifier, for: indexPath) as! CustomCell
+//        cell.imageView.image = UIImage(named: imageNames[indexPath.item])
         return cell
     }
     
@@ -87,18 +96,26 @@ extension CustomMenuBar: UICollectionViewDelegate, UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: self.frame.width / 4 , height: 55)
+        return CGSize(width: self.frame.width / 4 , height: 62)
         
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let x = CGFloat(indexPath.item) * customTabBarCollectionView.frame.width / 4 
+        indicatorViewLeadingConstraint.constant = x
         delegate?.customMenuBar(scrollTo: indexPath.row)
+        guard let cell = collectionView.cellForItem(at: indexPath) as? CustomCell else {return}
+        cell.imageView.image = cell.imageView.image?.withRenderingMode(.alwaysTemplate)
+        cell.imageView.tintColor = .init(red: 83/255, green: 35/255, blue: 178/255, alpha: 1)
     }
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        guard let cell = collectionView.cellForItem(at: indexPath) as? TabbarCell else {return}
-//        cell.label.textColor = .lightGray
+        guard let cell = collectionView.cellForItem(at: indexPath) as? CustomCell else {return}
+        cell.imageView.image = cell.imageView.image?.withRenderingMode(.alwaysTemplate)
+        cell.imageView.tintColor = .init(red: 108/255, green: 108/255, blue: 108/255, alpha: 1)
     }
+    
+    
 }
 //MARK:- UICollectionViewDelegateFlowLayout
 extension CustomMenuBar: UICollectionViewDelegateFlowLayout {
@@ -112,3 +129,43 @@ extension CustomMenuBar: UICollectionViewDelegateFlowLayout {
     }
 }
 
+extension CustomMenuBar {
+    func changeVC<T>(_ storyBoardIdentifier: String ,_ identifier: String) -> T? {
+        let storyboard = UIStoryboard.init(name: storyBoardIdentifier, bundle: .main)
+        guard let vc = storyboard.instantiateViewController(identifier: identifier) as? T else { return nil}
+        return vc
+    }
+}
+
+extension UIView {
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float)
+    {
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor =  backgroundCGColor
+    }
+}
+
+enum StoryboardNames: String {
+    case outgoing = "OutGoing"
+    case notice = "Notice"
+    case mypage = "MyPage"
+//    case schedule = "Schedule"
+    case login = "Login"
+}
+
+enum ViewControllerNames: String {
+    case outgoing = "OutGoingViewController"
+    case notice = "NoticeViewController"
+    case mypage = "MypageViewController"
+    case login = "LoginViewController"
+}
+
+
+//let vcNames = ["LoginViewController", "OutGoingViewController", "NoticeViewController", "MypageViewController"]
