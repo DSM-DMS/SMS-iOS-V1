62d1d6ede86c6f3f383b4c7c75b87c7fcc1e8823
이현욱 <hjhsha827@gmail.com>
2020-10-06 08:36:37 +0900

    [Modify] Modify UI

--- SMS_iOS_V1/SMS/SMS/CustomCell.swift
+++ SMS_iOS_V1/SMS/SMS/CustomCell.swift
@@ -1,11 +1,3 @@
-//
-//  CustomCell.swift
-//  CustomTabBar
-//
-//  Created by 이동건 on 2018. 4. 18..
-//  Copyright © 2018년 이동건. All rights reserved.
-//
-
 import UIKit
 
 class CustomCell: UICollectionViewCell {
