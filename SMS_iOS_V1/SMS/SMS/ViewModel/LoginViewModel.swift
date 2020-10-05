cba3e31358b596644ef1b41455b6a52275dbe754
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-05 17:07:42 +0900

    [Add] LoginViewModel

---
+++ SMS_iOS_V1/SMS/SMS/ViewModel/LoginViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewModel.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/10/05.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Alamofire
+
+class LoginViewModel {
+    
+}
