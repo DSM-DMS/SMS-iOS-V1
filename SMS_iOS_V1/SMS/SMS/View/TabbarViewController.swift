f2cf862a137ff6e1ba0e55269edc7aacd8b908b8
이현욱 <hjhsha827@gmail.com>
2020-10-05 19:32:03 +0900

    [Modify] TabbarViewController

--- SMS_iOS_V1/SMS/SMS/View/TabbarViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/TabbarViewController.swift
@@ -11,27 +11,25 @@ import UIKit
 class MainVC: UIViewController, CustomMenuBarDelegate{
     
     let loginVC = UIStoryboard(name: "Login", bundle: .main).instantiateViewController(identifier: "LoginViewController")
-    let outGoing = UIStoryboard(name: "OutGoing", bundle: .main).instantiateViewController(identifier: "OutGoingViewController")
-    let notice = UIStoryboard(name: "Notice", bundle: .main).instantiateViewController(identifier: "NoticeViewController")
-    let mypage = UIStoryboard(name: "MyPage", bundle: .main).instantiateViewController(identifier: "MypageViewController")
+    let outGoingVC = UIStoryboard(name: "OutGoing", bundle: .main).instantiateViewController(identifier: "OutGoingViewController")
+    let noticeVC = UIStoryboard(name: "Notice", bundle: .main).instantiateViewController(identifier: "NoticeViewController")
+    let mypageVC = UIStoryboard(name: "MyPage", bundle: .main).instantiateViewController(identifier: "MypageViewController")
+
+    lazy var vcArr = [loginVC, outGoingVC, noticeVC, mypageVC]
     
-    lazy var vcArr = [loginVC, outGoing, notice, mypage]
-    
-    
-    
-    //MARK: Outltes
     var pageCollectionView: UICollectionView = {
         let collectionViewLayout = UICollectionViewFlowLayout()
         collectionViewLayout.scrollDirection = .horizontal
         let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 100, height: 100), collectionViewLayout: collectionViewLayout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
         return collectionView
     }()
+    
     var customMenuBar = CustomMenuBar()
-    //MARK: Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.hidesBarsOnSwipe = true
         setupCustomTabBar()
         setupPageCollectionView()
     }
@@ -40,13 +38,14 @@ class MainVC: UIViewController, CustomMenuBarDelegate{
         self.view.addSubview(customMenuBar)
         customMenuBar.delegate = self
         customMenuBar.translatesAutoresizingMaskIntoConstraints = false
-        customMenuBar.indicatorViewWidthConstraint.constant = self.view.frame.width / 6
+        customMenuBar.indicatorViewWidthConstraint.constant = self.view.frame.width / 8 
         customMenuBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         customMenuBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         customMenuBar.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         customMenuBar.heightAnchor.constraint(equalToConstant: 62).isActive = true
         customMenuBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         customMenuBar.clipsToBounds = false
+        customMenuBar.indicatorViewLeadingConstraint.constant = 25
     }
     
     func customMenuBar(scrollTo index: Int) {
@@ -57,7 +56,6 @@ class MainVC: UIViewController, CustomMenuBarDelegate{
     func setupPageCollectionView(){
         pageCollectionView.delegate = self
         pageCollectionView.dataSource = self
-        pageCollectionView.backgroundColor = .gray
         pageCollectionView.showsHorizontalScrollIndicator = false
         pageCollectionView.isPagingEnabled = true
         pageCollectionView.register(UINib(nibName: PageCell.reusableIdentifier, bundle: nil), forCellWithReuseIdentifier: PageCell.reusableIdentifier)
@@ -66,13 +64,19 @@ class MainVC: UIViewController, CustomMenuBarDelegate{
         pageCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         pageCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         pageCollectionView.bottomAnchor.constraint(equalTo: self.customMenuBar.topAnchor).isActive = true
-        pageCollectionView.backgroundColor = .red
+        for vc in vcArr {
+            self.addChild(vc)
+            vc.didMove(toParent: self)
+            self.view.addSubview(vc.view)
+            vc.view.frame = CGRect.init(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height - 100)
+        }
     }
 }
 //MARK:- UICollectionViewDelegate, UICollectionViewDataSource
 extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PageCell.reusableIdentifier, for: indexPath) as! PageCell
+        cell.contentView.addSubview(vcArr[indexPath.row].view)
         return cell
     }
     
@@ -81,18 +85,9 @@ extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource {
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        customMenuBar.indicatorViewLeadingConstraint.constant = scrollView.contentOffset.x / 4
+        customMenuBar.indicatorViewLeadingConstraint.constant = scrollView.contentOffset.x / 4 + 25
     }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-//        let vc = vcArr[indexPath.row]
-//        vc.view.frame = UIApplication.shared.windows[0].frame
-//        vc.didMove(toParent: self)
-//        self.addChild(vc)
-//        self.view.addSubview(vc.view)
-    }
-//
+
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         let itemAt = Int(targetContentOffset.pointee.x / self.view.frame.width)
         let indexPath = IndexPath(item: itemAt, section: 0)
@@ -104,22 +99,39 @@ extension MainVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: pageCollectionView.frame.width, height: pageCollectionView.frame.height)
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
 }
-
-extension MainVC: PageCellProtocol {
-    func willAdd(_ childViewController: UIViewController, at index: Int) {
-        print(1)
-    }
-    
-    
-}
-
 
 extension NSObject {
     static var reusableIdentifier: String {
         return String(describing: self)
     }
 }
+
+extension MainVC {
+    func changeVC<T>(_ storyBoardIdentifier: StoryboardNames ,_ identifier: ViewControllerNames) -> T? {
+        let storyboard = UIStoryboard.init(name: storyBoardIdentifier.rawValue, bundle: .main)
+        guard let vc = storyboard.instantiateViewController(identifier: identifier.rawValue) as? T else { return nil}
+        return vc
+    }
+    
+    enum StoryboardNames: String {
+        case outgoing = "OutGoing"
+        case notice = "Notice"
+        case mypage = "MyPage"
+    //    case schedule = "Schedule"
+        case login = "Login"
+    }
+
+    enum ViewControllerNames: String {
+        case outgoing = "OutGoingViewController"
+        case notice = "NoticeViewController"
+        case mypage = "MypageViewController"
+        case login = "LoginViewController"
+    }
+}
+
+
