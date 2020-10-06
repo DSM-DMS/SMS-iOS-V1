03b0092254445837314994a81ec51a7b3a9af73e
이현욱 <hjhsha827@gmail.com>
2020-10-06 20:26:35 +0900

    [Add] UIView Extension

--- SMS_iOS_V1/SMS/SMS/Extension/UIView.swift
+++ SMS_iOS_V1/SMS/SMS/Extension/UIView.swift
@@ -1,11 +1,3 @@
-//
-//  UIView.swift
-//  SMS
-//
-//  Created by 이현욱 on 2020/10/05.
-//  Copyright © 2020 DohyunKim. All rights reserved.
-//
-
 import UIKit
 
 extension UIView {
@@ -13,5 +5,27 @@ extension UIView {
         for view in views {
             self.addSubview(view)
         }
+    }
+    
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor =  backgroundCGColor
+    }
+}
+
+extension UIColor {
+    static var customPurple: UIColor {
+        return UIColor(displayP3Red: 83/255, green: 35/255, blue: 178/255, alpha: 1)
+    }
+    
+    static var customBlack: UIColor {
+        return UIColor(displayP3Red: 108/255, green: 108/255, blue: 108/255, alpha: 1)
     }
 }
