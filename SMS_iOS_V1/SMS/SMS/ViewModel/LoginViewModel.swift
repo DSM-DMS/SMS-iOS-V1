ad0c8dbd6455877ed8cfc02c4b3bc817e15eef35
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-06 21:15:03 +0900

    [Add] func request

--- SMS_iOS_V1/SMS/SMS/ViewModel/LoginViewModel.swift
+++ SMS_iOS_V1/SMS/SMS/ViewModel/LoginViewModel.swift
@@ -13,4 +13,28 @@ import Alamofire
 
 class LoginViewModel {
     
+    let view = LoginViewController()
+    let url = URL(string: "")
+    
+    func request() {
+        let id = view.IDTextField.text!
+        let pw = view.PWTextField.text!
+        
+        let param = ["id":id, "pw":pw]
+        
+        let header = ["application/json":"content-type"]
+        
+        Alamofire.request(url!, method: .post, parameters: param, encoding: JSONEncoding.default, headers: header).responseJSON {
+            (response) in
+            switch response.result {
+            case .success(_) :
+                if let json = response.value {
+                   json as! [String:AnyObject]
+                }
+            }
+        }
+        
+
+    }
+    
 }
