92b6a2803aad4fc6cf82cd9ad80ea3cdc2978b25
이현욱 <hjhsha827@gmail.com>
2020-10-14 23:28:26 +0900

    [Add] APIService

---
+++ SMS_iOS_V1/SMS/SMS/Network/APIService.swift
@@ -0,0 +1,39 @@
+//
+//  APIService.swift
+//  SMS
+//
+//  Created by 이현욱 on 2020/10/13.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+import Alamofire
+import RxSwift
+import RxCocoa
+
+protocol APIServiceProtocol {
+    func get()
+    func post()
+    func patch()
+}
+
+class APIService  {
+    private let baseUrl = ""
+   
+//    func get() {
+//        <#code#>
+//    }
+//
+//    func post() {
+//        <#code#>
+//    }
+//
+//    func patch() {
+//        <#code#>
+//    }
+//
+    
+    
+    
+}
