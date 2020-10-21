a5a611a02a63200f7a22344c85e1a5406c12a0ff
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-21 19:59:38 +0900

    [Delete] ButtonExtention

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingViewController.swift
@@ -10,41 +10,14 @@ import UIKit
 
 class OutGoingViewController: UIViewController {
     
-    @IBOutlet weak var outGoingApplyButton: UIButton!
-    @IBOutlet weak var outGoingLogButton: UIButton!
-    @IBOutlet weak var outGoingNoticeButton: UIButton!
-    @IBOutlet weak var outGoingDeedButton: UIButton!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        shadowSetting()
+        
     }
     
 }
 
  
-extension OutGoingViewController {
-    func shadowSetting() {
-        outGoingApplyButton.addShadow(offset: CGSize(width: 0, height: 2),
-                      color: .lightGray,
-                      shadowRadius: 2,
-                      opacity: 0.7,
-                      cornerRadius: 5)
-        outGoingLogButton.addShadow(offset: CGSize(width: 0, height: 2),
-                                    color: .lightGray,
-                                    shadowRadius: 2,
-                                    opacity: 0.7,
-                                    cornerRadius: 5)
-        outGoingNoticeButton.addShadow(offset: CGSize(width: 0, height: 2),
-                                       color: .lightGray,
-                                       shadowRadius: 2,
-                                       opacity: 0.7,
-                                       cornerRadius: 5)
-        outGoingDeedButton.addShadow(offset: CGSize(width: 0, height: 2),
-                                     color: .lightGray,
-                                     shadowRadius: 2,
-                                     opacity: 0.7,
-                                     cornerRadius: 5)
-    }
 
-}
+
