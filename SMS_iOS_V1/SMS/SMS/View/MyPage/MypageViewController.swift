8b7d320e1fd5c66d88c6b2594235e16c35e766c3
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-23 19:41:11 +0900

    [Modify] MypageViewController

--- SMS_iOS_V1/SMS/SMS/View/MyPage/MypageViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/MyPage/MypageViewController.swift
@@ -10,28 +10,10 @@ import UIKit
 
 class MypageViewController: UIViewController {
     
-//    @IBOutlet weak var pwChangeButton: UIButton!
-//    @IBOutlet weak var logOutButton: UIButton!
-//    @IBOutlet weak var introduceDevButton: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        pwChangeButton.addShadow(offset: CGSize(width: 0, height: 2),
-//                                 color: .lightGray,
-//                                 shadowRadius: 2,
-//                                 opacity: 0.7,
-//                                 cornerRadius: 10)
-//        logOutButton.addShadow(offset: CGSize(width: 0, height: 2),
-//                               color: .lightGray,
-//                               shadowRadius: 2,
-//                               opacity: 0.7,
-//                               cornerRadius: 10)
-//        introduceDevButton.addShadow(offset: CGSize(width: 0, height: 2),
-//                                     color: .lightGray,
-//                                     shadowRadius: 2,
-//                                     opacity: 0.7,
-//                                     cornerRadius: 10)
+        
     }
     
 }
