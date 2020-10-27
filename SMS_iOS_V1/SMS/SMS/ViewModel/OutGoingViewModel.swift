f921601bd6b246a1cc1bdc5185cc7d2638fb7d27
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-27 17:12:05 +0900

    [Add] presenting functions

--- SMS_iOS_V1/SMS/SMS/ViewModel/OutGoingViewModel.swift
+++ SMS_iOS_V1/SMS/SMS/ViewModel/OutGoingViewModel.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2020 DohyunKim. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class OutGoingViewModel {
+    
+    let rootViewController = StoryBoard.OutGoing.viewController
+    
+    func presentingOutGoingApplyViewController() {
+        let presentViewController = StoryBoard.OutGoingApply.viewController
+        presentViewController.modalPresentationStyle = .fullScreen
+        rootViewController.present(presentViewController, animated: true, completion: nil)
+    }
+    
+    func presentingOutGoingLogViewController() {
+        let presentingViewController = StoryBoard.OutGoingLog.viewController
+        presentingViewController.modalPresentationStyle = .fullScreen
+        rootViewController.present(presentingViewController, animated: true, completion: nil)
+    }
+    
+    func presentingOutGoingDeedViewController() {
+        let presentingViewController = StoryBoard.OutGoingDeed.viewController
+        presentingViewController.modalPresentationStyle = .fullScreen
+        rootViewController.present(presentingViewController, animated: true, completion: nil)
+    }
+    
+    func presentingOutGoingNoticeViewController() {
+        let presentingViewController = StoryBoard.OutGoingNotice.viewController
+        presentingViewController.modalPresentationStyle = .fullScreen
+        rootViewController.present(presentingViewController, animated: true, completion: nil)
+    }
+}
