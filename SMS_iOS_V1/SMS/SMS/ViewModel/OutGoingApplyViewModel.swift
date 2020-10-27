057dffff761060c78cf04d3c3260fd36e99524d0
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-27 18:56:20 +0900

    [Add] OutGoingApplyViewModel

---
+++ SMS_iOS_V1/SMS/SMS/ViewModel/OutGoingApplyViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  OutGoingApplyViewModel.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/10/27.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class OutGoingApplyViewModel {
+    
+    
+}
