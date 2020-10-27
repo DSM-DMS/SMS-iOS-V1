2e72308df6fff0db0108e1d54b9aab1773f540ef
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-27 19:57:51 +0900

    [Modify] OutgoingAlertViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingAlertViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingAlertViewController.swift
@@ -12,6 +12,8 @@ class OutGoingAlertViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIButton!
     
+    let viewModel = OutGoingAlertViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.addShadow(offset: CGSize(width: 0, height: 2),
@@ -19,5 +21,11 @@ class OutGoingAlertViewController: UIViewController {
                                  shadowRadius: 2,
                                  opacity: 0.7,
                                  cornerRadius: 10)
+    }
+    @IBAction func cancelButton(_ sender: Any) {
+        viewModel.dismissViewController()
+    }
+    @IBAction func applyButton(_ sender: Any) {
+        viewModel.presentViewController()
     }
 }
