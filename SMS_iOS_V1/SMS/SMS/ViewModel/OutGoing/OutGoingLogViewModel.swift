1b937cd0e29aaa94b1182a5503343f864ee8a225
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-29 11:32:04 +0900

    [Add] OutGoingLogViewModel

---
+++ SMS_iOS_V1/SMS/SMS/ViewModel/OutGoing/OutGoingLogViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  OutGoingLogViewModel.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/10/29.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class OutGoingLogViewModel {
+    
+    
+}
