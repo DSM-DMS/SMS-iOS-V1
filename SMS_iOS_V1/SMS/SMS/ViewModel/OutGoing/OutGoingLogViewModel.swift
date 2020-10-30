3b37ef956db867bcd392cc3d060b9c1b02282984
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-30 22:00:47 +0900

    [Add] dismissing functions

--- SMS_iOS_V1/SMS/SMS/ViewModel/OutGoing/OutGoingLogViewModel.swift
+++ SMS_iOS_V1/SMS/SMS/ViewModel/OutGoing/OutGoingLogViewModel.swift
@@ -10,5 +10,8 @@ import UIKit
 
 class OutGoingLogViewModel {
     
-    
+    func dismissongViewController() {
+        let rootViewController = StoryBoard.OutGoingLog.viewController
+        rootViewController.dismiss(animated: true, completion: nil)
+    }
 }
