ccfdbdafdca4d3f191c526bfc98f2600a712ce40
이현욱 <hjhsha827@gmail.com>
2020-12-04 19:14:28 +0900

    [Add] CustomShadowButton

---
+++ SMS_iOS_V1/SMS/SMS/Extension/CustomShadowButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomShadowButton.swift
+//  SMS
+//
+//  Created by 이현욱 on 2020/12/04.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class CustomShadowButton: UIButton {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 5.0;
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowRadius = 2
+        self.layer.shadowOpacity = 0.7
+    }
+}
