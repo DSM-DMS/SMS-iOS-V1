69f7004aad077f11a43d72fdcf3fff008a3ad247
이현욱 <hjhsha827@gmail.com>
2020-12-07 23:30:34 +0900

    [Modify] AppDelegate

--- SMS_iOS_V1/SMS/SMS/Supporting/AppDelegate.swift
+++ SMS_iOS_V1/SMS/SMS/Supporting/AppDelegate.swift
@@ -2,20 +2,28 @@
 //  AppDelegate.swift
 //  SMS
 //
-//  Created by DohyunKim on 2020/09/04.
-//  Copyright © 2020 DohyunKim. All rights reserved.
+//  Created by 이현욱 on 2020/09/03.
+//  Copyright © 2020 이현욱. All rights reserved.
 //
 
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
+    
+    var coordinator: AppCoordinator?
+    var window:UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let nav = UINavigationController()
+        coordinator = AppCoordinator(nav: nav)
+        coordinator?.start()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        
         // Override point for customization after application launch.
         return true
     }
 }
-
