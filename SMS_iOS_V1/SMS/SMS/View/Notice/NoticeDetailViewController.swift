091af44938b21304fee88df804734a5ce1d1c291
이현욱 <hjhsha827@gmail.com>
2020-12-11 08:50:04 +0900

    [Add] NoticeDetailViewController

---
+++ SMS_iOS_V1/SMS/SMS/View/Notice/NoticeDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NoticeDetailViewController.swift
+//  SMS
+//
+//  Created by DohyunKim on 2020/10/23.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+class NoticeDetailViewController: UIViewController, Storyboarded {
+    let disposeBag = DisposeBag()
+    weak var coordinator: NoticeCoordinator?
+    
+    @IBOutlet weak var popVCBtn: UIButton!
+    @IBOutlet weak var viewsLbl: UILabel!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension NoticeDetailViewController {
+    private func bindAction() {
+//        popVCBtn.rx.tap
+//            .map { self.coordinator?.dis }
+//            .subscribe()
+//            .disposed(by: disposeBag)
+    }
+}
