8bf63243f356dcc62015478fe9ec08bc6c87e344
이현욱 <hjhsha827@gmail.com>
2020-12-14 11:01:53 +0900

    [Modify] OutGoingApplyViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingApplyViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingApplyViewController.swift
@@ -7,25 +7,61 @@
 //
 
 import UIKit
-class OutGoingApplyViewController: UIViewController, Storyboarded {
-    weak var coordinator: OutGoingCoordinator?
 
-    @IBOutlet weak var applyButton: CustomShadowButton!
-    
+import RxSwift
+import RxCocoa
+
+class OutGoingApplyViewController: UIViewController, Storyboarded {
+    let disposeBag = DisposeBag()
+    weak var coordinator: OutGoingCoordinator?
     let viewModel = OutGoingApplyViewModel()
-    
-//    weak var coordinator: OutGoingCoordinator?
-    
+
+    @IBOutlet weak var popVCBtn: UIButton!
+    @IBOutlet weak var diseaseBtn: UIButton!
+    @IBOutlet weak var placeTextField: UITextField!
+    @IBOutlet weak var startTimeTextField: UITextField!
+    @IBOutlet weak var endTimeTextField: UITextField!
+    @IBOutlet weak var outReasonTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var applyButton: CustomShadowButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
+        bindAction()
+    }
+}
+
+extension OutGoingApplyViewController {
+    private func bind() {
+        let input = OutGoingApplyViewModel.Input
+            .init(dateDriver: dateTextField.rx.text.orEmpty.asDriver(),
+                  reasonDriver: outReasonTextField.rx.text.orEmpty.asDriver(),
+                  startTimeDriver: startTimeTextField.rx.text.orEmpty.asDriver(),
+                  endTimeDriver: endTimeTextField.rx.text.orEmpty.asDriver(),
+                  placeDriver: placeTextField.rx.text.orEmpty.asDriver(),
+                  diseaseDriver: diseaseBtn.rx.tap.asDriver(),
+                  applyDriver: applyButton.rx.tap.asDriver())
+        
+        let output = viewModel.transform(input)
+        
+        output.response.subscribe { model in
+            switch model.code {
+            case 200:
+                self.coordinator?.outGoingCompleted()
+            case 400:
+                self.applyButton.shake()
+            default:
+                break
+            }
+        } onError: { _ in
+            self.applyButton.shake()
+        }.disposed(by: disposeBag)
     }
     
-    @IBAction func applyButton(_ sender: Any) {
-//        viewModel.presentingViewController()
-        
-    }
-    @IBAction func backButton(_ sender: Any) {
-//        viewModel.dismissingViewController()
+    private func bindAction() {
+        popVCBtn.rx.tap
+            .bind {self.coordinator?.pop()}
+            .disposed(by: disposeBag)
     }
 }
