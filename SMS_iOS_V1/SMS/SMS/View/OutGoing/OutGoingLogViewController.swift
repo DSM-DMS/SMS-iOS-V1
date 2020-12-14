e419d5da849704eeed282da9ad7035afdbfe3c28
이현욱 <hjhsha827@gmail.com>
2020-12-14 11:00:57 +0900

    [Modify] OutGoingLogViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingLogViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingLogViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 class OutGoingLogViewController: UIViewController, Storyboarded {
+    let disposeBag = DisposeBag()
     weak var coordinator: OutGoingCoordinator?
+    //    let viewModel = OutGoingLogViewModel()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindUI()
+    }
+}
+
+extension OutGoingLogViewController {
+    private func bindUI() {
+        let logs: Observable<OutGoingLogModel> = SMSAPIClient.shared.networking(from: .lookUpAllOuting(""))
         
+        logs.map { [$0] }
+            .bind(to: tableView.rx.items(cellIdentifier: OutGoingLogTableViewCell.NibName, cellType: OutGoingLogTableViewCell.self)) { idx, log, cell in
+//                cell.dateLbl.text = log.
+                cell.placeLbl.text = log.place
+                cell.reasonLbl.text = log.reason
+            
+//                switch log.status {
+//
+//                }
+            }.disposed(by: disposeBag)
     }
 }
