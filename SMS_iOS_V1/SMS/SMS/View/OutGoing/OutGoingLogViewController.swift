92f29dd0d4a756dfe4a2abd84e0a5c493c756d84
이현욱 <hjhsha827@gmail.com>
2020-12-14 23:52:01 +0900

    [Modify] OutGoingLogViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingLogViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingLogViewController.swift
@@ -14,30 +14,32 @@ import RxSwift
 class OutGoingLogViewController: UIViewController, Storyboarded {
     let disposeBag = DisposeBag()
     weak var coordinator: OutGoingCoordinator?
-    //    let viewModel = OutGoingLogViewModel()
     
     @IBOutlet weak var tableView: UITableView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bindUI()
+        tableView.rowHeight = 120
     }
 }
 
 extension OutGoingLogViewController {
     private func bindUI() {
-        let logs: Observable<OutGoingLogModel> = SMSAPIClient.shared.networking(from: .lookUpAllOuting(""))
+        let logs: Observable<OutGoingLogModel> = SMSAPIClient.shared.networking(from: .lookUpAllOuting)
         
-        logs.map { [$0] }
-            .bind(to: tableView.rx.items(cellIdentifier: OutGoingLogTableViewCell.NibName, cellType: OutGoingLogTableViewCell.self)) { idx, log, cell in
-//                cell.dateLbl.text = log.
-                cell.placeLbl.text = log.place
-                cell.reasonLbl.text = log.reason
+        logs.map { ($0.outings ?? []) }.bind(to: tableView.rx.items(cellIdentifier: OutGoingLogTableViewCell.NibName, cellType: OutGoingLogTableViewCell.self)) { idx, log, cell in
+            cell.startTimeLbl.text = UnixStampToDate(with: log.start_time)
+            cell.endTimeLbl.text = UnixStampToDate(with: log.end_time)
+            cell.placeLbl.text = log.place
+            cell.reasonLbl.text = log.reason
+            switch Int(log.outing_status) {
             
-//                switch log.status {
-//
-//                }
-            }.disposed(by: disposeBag)
+            case .none:
+                <#code#>
+            case .some(_):
+                <#code#>
+            }
+        }.disposed(by: disposeBag)
     }
 }
