b13f2327afb3c18cbe534aaf1ef1db1ba8768109
이현욱 <hjhsha827@gmail.com>
2020-12-14 23:53:03 +0900

    [Modify] OutGoingLogModel

--- SMS_iOS_V1/SMS/SMS/Model/OutGoing/OutGoingLogModel.swift
+++ SMS_iOS_V1/SMS/SMS/Model/OutGoing/OutGoingLogModel.swift
@@ -9,24 +9,30 @@
 import UIKit
 
 struct OutGoingLogModel: Codable {
-    let statusCode: Int
     let code: Int
     let message: String
-    let uuid: String
+    let outings: [Outings]?
+    let status: Int
+}
+
+struct Outings: Codable {
+    let outing_uuid: String
     let place: String
     let reason: String
-    let startTime: Int
-    let endTime: Int
-    let situation: String
-    let status: String
+    let start_time: Int
+    let end_time: Int
+    let outing_situation: String
+    let outing_status: String
     
-    enum CodingKeys: String, CodingKey {
-        case uuid = "outing_uuid"
-        case startTime = "start_time"
-        case endTime = "end_time"
-        case situation = "outing_situation"
-        case status = "outing_status"
-    }
+//    enum CodingKeys: String, CodingKey {
+//        case uuid = "outing_uuid"
+//        case startTime = "start_time"
+//        case endTime = "end_time"
+//        case situation = "outing_situation"
+//        case status = "outing_status"
+//        case place
+//        case reason
+//    }
 }
 
 enum OutingState {
