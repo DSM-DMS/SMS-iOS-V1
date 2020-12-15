29457db944fe83c61c887e1337af2c4433d5e20e
이현욱 <hjhsha827@gmail.com>
2020-12-15 17:28:16 +0900

    [Modify] OutGoingLogViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingLogViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingLogViewController.swift
@@ -11,17 +11,24 @@ import UIKit
 import RxCocoa
 import RxSwift
 
-class OutGoingLogViewController: UIViewController, Storyboarded {
+class OutGoingLogViewController: UIViewController, Storyboarded, UIScrollViewDelegate, UITableViewDelegate {
     let disposeBag = DisposeBag()
     weak var coordinator: OutGoingCoordinator?
     
+    @IBOutlet weak var popVCBtn: UIButton!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bindUI()
         tableView.rowHeight = 120
+        tableView.rx.setDelegate(self)
     }
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//           return 50
+//       } //믿어보게
+    
+    
 }
 
 extension OutGoingLogViewController {
@@ -33,13 +40,32 @@ extension OutGoingLogViewController {
             cell.endTimeLbl.text = UnixStampToDate(with: log.end_time)
             cell.placeLbl.text = log.place
             cell.reasonLbl.text = log.reason
-            switch Int(log.outing_status) {
             
-            case .none:
-                <#code#>
-            case .some(_):
-                <#code#>
+            switch Int(log.outing_status) {
+            case 0:
+                self.cellState(cell: cell, text: "승인거부", color: .customRed)
+            case 1:
+                self.cellState(cell: cell, text: "승인대기", color: .customYellow)
+            case 2:
+                self.cellState(cell: cell, text: "승인완료", color: .customGrenn)
+            case 3:
+                self.cellState(cell: cell, text: "외출중", color: .customOrange)
+            case 4:
+                self.cellState(cell: cell, text: "만료", color: .customPurple)
+            default:
+                self.cellState(cell: cell, text: "에러", color: .customBlack)
             }
         }.disposed(by: disposeBag)
+        
+        popVCBtn.rx.tap
+            .bind { self.coordinator?.pop() }
+            .disposed(by: disposeBag)
+        
+    }
+    
+    func cellState(cell: OutGoingLogTableViewCell,text: String, color: UIColor) -> OutGoingLogTableViewCell {
+        cell.outGoingStateLbl.text = text
+        cell.statusColorView.backgroundColor = color
+        return cell
     }
 }
