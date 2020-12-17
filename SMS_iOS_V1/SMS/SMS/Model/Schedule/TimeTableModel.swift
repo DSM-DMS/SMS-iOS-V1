4d01e75dbc5186512b9747d99399c852073e9f2f
이현욱 <hjhsha827@gmail.com>
2020-12-17 23:51:40 +0900

    [Modify] TimeTableModel

--- SMS_iOS_V1/SMS/SMS/Model/Schedule/TimeTableModel.swift
+++ SMS_iOS_V1/SMS/SMS/Model/Schedule/TimeTableModel.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+struct TimeTableModel: Codable {
+    let status: Int
+    let code: Int
+    let message: String
+    let time1:String
+    let time2:String
+    let time3:String
+    let time4:String
+    let time5:String
+    let time6:String
+    let time7:String
+}
