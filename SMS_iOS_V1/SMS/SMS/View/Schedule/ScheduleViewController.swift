2cba726c71722ee331ae7b9cf31be0f8c9267092
이현욱 <hjhsha827@gmail.com>
2020-12-17 23:49:23 +0900

    [Modify] ScheduleViewController

--- SMS_iOS_V1/SMS/SMS/View/Schedule/ScheduleViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/Schedule/ScheduleViewController.swift
@@ -4,21 +4,17 @@ import FSCalendar
 import RxSwift
 import RxCocoa
 
-class ScheduleViewController: UIViewController {
-    // 한 이벤트에 대해 시작한 날짜와 끝난 날짜 -> 영화토이처럼 배열로 만들어
-    // 날짜를 여러번 호출, 비슷비슷한 함수가 많이 사용되는데 이걸 줄일 방법을 찾아보고 싶음
+class ScheduleViewController: UIViewController, Storyboarded, FSCalendarDelegate, FSCalendarDataSource, UICollectionViewDelegate {
     
-    private var bool = false
-//    let observable: BehaviorRelay<ScheduleModel> = .init(value: ScheduleModel(date: "", eventInfo: "", eventColor: .red))
-    private let date: BehaviorRelay<Date> = .init(value: Date())
-    private let disposeBag = DisposeBag()
-    private lazy var dateFormatter = globalDateFormatter(formType(rawValue: formType.day.rawValue)!)
-    private var firstDate: Date? = Date()
+    let date = globalDateFormatter(.untilDay, Date())
+    lazy var yearDayArr = date.dropLast().components(separatedBy: "-").map { Int($0)! }
+    lazy var a: Observable<ScheduleModel> = getSchedule(with: yearDayArr[0], with: yearDayArr[1])
+    var value = false
+    weak var coordinator: ScheduleCoordinator?
+    let disposeBag = DisposeBag()
     
-    @IBOutlet weak var xibViewTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var nextMonthBtn: UIButton!
     @IBOutlet weak var previousMonthBtn: UIButton!
-    @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var changeViewBtn: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var calendarView: FSCalendar!
@@ -26,41 +22,96 @@ class ScheduleViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.calendarSetting()
         self.tableViewSetting()
         bindAction()
         bindUI()
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        coordinator?.disappear()
+    }
 }
-
+/*
+ 
+ {
+ "code": 0,
+ "message": "succeed to get schedules with year and month",
+ "schedules": [
+ {
+ "detail": "세부 내용을 입력해야만 알겠어?",
+ "end_date": 1608166860000,
+ "schedule_uuid": "schedule-3724375272514",
+ "start_date": 1607994060000
+ },
+ {
+ "detail": "심장은 뜨겁게 뇌는 차갑게",
+ "end_date": 1608598860000,
+ "schedule_uuid": "schedule-7722837206942",
+ "start_date": 1608598860000
+ },
+ {
+ "detail": "밥 먹자!",
+ "end_date": 1608080460000,
+ "schedule_uuid": "schedule-7898672256913",
+ "start_date": 1607994060000
+ },
+ {
+ "detail": "야외 촬영이 있는 날입니다. 군복 챙겨오세요.",
+ "end_date": 1608080460000,
+ "schedule_uuid": "schedule-9121050753075",
+ "start_date": 1607994060000
+ },
+ {
+ "detail": "공휴일",
+ "end_date": 1608253260000,
+ "schedule_uuid": "schedule-9403808019852",
+ "start_date": 1608166860000
+ }
+ ],
+ "status": 200
+ }
+ */
 
 //MARK- extension
-
 extension ScheduleViewController {
+    private func bindUI() {
+        a.map { ($0.schedules ?? []) }
+        //            .bind(to: calendarView.collectionView.rx.items(cellIdentifier: CalendarCollectionViewCell.NibName, cellType: CalendarCollectionViewCell.self)) { idx, schedule, cell in
+        //
+        //            }.disposed(by: disposeBag)
+        //            .map { schedules in
+        //                for i in schedules.enumerated() {
+        //                    UnixStampToDate(with: i.element.startTime)
+        //
+        //                }
+        //            }
+    }
+    
     private func bindAction() {
-        changeViewBtn.rx.tap
-            .map { self.changeHidden(value: self.bool) }
-            .map { self.bool.toggle() }
-            .subscribe().disposed(by: disposeBag)
-        
         Observable.merge(previousMonthBtn.rx.tap.map {
             Calendar.current.date(byAdding: .month, value: -1, to: self.calendarView.currentPage)!
         }, nextMonthBtn.rx.tap.map {
             Calendar.current.date(byAdding: .month, value: +1, to: self.calendarView.currentPage)!
-        }).map { self.calendarView.setCurrentPage($0, animated: true)
-            self.yearLabel.text = globalDateFormatter(formType(rawValue: formType.month.rawValue)!).string(from: self.calendarView.currentPage)
+        })
+        .map { date in
+            self.calendarView.setCurrentPage(date, animated: true)
+            
         }
-        .subscribe().disposed(by: disposeBag)
+        
+        //        .disposed(by: disposeBag)
+        
+        changeViewBtn.rx.tap
+            .bind { _ in
+                self.value.toggle()
+                self.changeHidden(value: self.value)
+            }
+            .disposed(by: disposeBag)
     }
     
-    private func bindUI() {
-//        observable.map { [$0] } // ScheduleModel 재대로 나오면 다시
-//            .bind(to: tableView.rx.items(cellIdentifier: "ScheduleCell", cellType: ScheduleCell.self)) { idx, schedule, cell in
-//                cell.scheduleDateLbl.text = schedule.date
-//                cell.scheduleInfoLbl.text = schedule.eventInfo
-//                cell.scheduleColorView.backgroundColor = schedule.eventColor
-//            }.disposed(by: disposeBag)
+    func getSchedule(with year: Int, with month: Int) -> Observable<ScheduleModel> {
+        return SMSAPIClient.shared.networking(from: .schedules(year, month))
     }
     
     private func tableViewSetting() {
@@ -73,16 +124,26 @@ extension ScheduleViewController {
                             shadowRadius: 3,
                             opacity: 0.5,
                             cornerRadius: 17)
-        self.yearLabel.text = globalDateFormatter(formType(rawValue: formType.month.rawValue)!).string(from: calendarView.currentPage)
+        
+        a.map { $0.schedules ?? [] }
+            .bind(to: tableView.rx.items(cellIdentifier: ScheduleCell.NibName, cellType: ScheduleCell.self)) { idx, schedule, cell in
+                cell.scheduleDateLbl.text = UnixStampToStringDate(with: schedule.startTime) + " ~ " + UnixStampToStringDate(with: schedule.endTime)
+                cell.scheduleInfoLbl.text = schedule.detail
+                
+            }.disposed(by: disposeBag)
+        
     }
-    
-    private func changeHidden(value: Bool) {
-        yearLabel.isHidden = !value
-        previousMonthBtn.isHidden = !value
-        nextMonthBtn.isHidden = !value
-        calendarView.isHidden = !value
-        tableView.isHidden = !value
-        timeScheduleView.isHidden = value
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // 어떻게 쓸지 고민
+        cell.layer.masksToBounds = true
+        let count = 2
+        if count == 2 {
+            cell.layer.cornerRadius = 17
+            cell.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        } else if indexPath.row == 1 {
+            cell.layer.cornerRadius = 17
+            cell.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        }
     }
     
     private func calendarSetting() {
@@ -90,48 +151,53 @@ extension ScheduleViewController {
         calendarView.appearance.caseOptions = [.headerUsesUpperCase, .weekdayUsesSingleUpperCase]
         calendarView.today = nil
         calendarView.placeholderType = .none
-        calendarView.delegate = self
-        calendarView.dataSource = self
         timeScheduleView.isHidden = true
         calendarView.register(CalendarCollectionViewCell.self, forCellReuseIdentifier: "cell")
-        self.changeViewBtn.addShadow(offset: CGSize(width: 0, height: 2.5),
-                                     color: .gray,
-                                     shadowRadius: 2,
-                                     opacity: 0.5,
-                                     cornerRadius: 25)
+        calendarView.appearance.headerTitleFont = UIFont.boldSystemFont(ofSize: 17)
+        calendarView.appearance.headerDateFormat = formType.month.rawValue
+        calendarView.appearance
+    }
+    
+    private func changeHidden(value: Bool) {
+        previousMonthBtn.isHidden = !value
+        nextMonthBtn.isHidden = !value
+        calendarView.isHidden = !value
+        tableView.isHidden = !value
+        timeScheduleView.isHidden = value
     }
 }
 
-extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegateAppearance {
-    private func configureVisibleCells() {
-        calendarView.visibleCells().forEach { (cell) in
-            let date = calendarView.date(for: cell)
-            let position = calendarView.monthPosition(for: cell)
-            self.configure(cell: cell, for: date!, at: position)
-        }
-    }
+
+extension ScheduleViewController {
+    //    private func configureVisibleCells() {
+    //        calendarView.visibleCells().forEach { (cell) in
+    //            let date = calendarView.date(for: cell)
+    //            let position = calendarView.monthPosition(for: cell)
+    //            self.configure(cell: cell, for: date!, at: position)
+    //        }
+    //    }
     
     private func configure(cell: FSCalendarCell, for date: Date, at position: FSCalendarMonthPosition) {
-//        let calCell = cell as! CalendarCollectionViewCell
-//        var selectionType = SelectionType.none
-//
-//        if combineArray.contains(strDate){
-//            if (combineArray.contains(previousDate) && combineArray.contains(nextDate)) {
-//                selectionType = .middle
-//            } else if combineArray.contains(previousDate) && combineArray.contains(strDate) {
-//                selectionType = .rightBorder
-//            } else if combineArray.contains(nextDate) {
-//                selectionType = .leftBorder
-//            } else {
-//                selectionType = .single
-//            }
-//        }
-//        if selectionType == .none {
-//            calCell.eventLayer!.isHidden = true
-//            return
-//        }
-//        calCell.eventLayer!.isHidden = false
-//        calCell.selectionType = selectionType
+        //        let calCell = cell as! CalendarCollectionViewCell
+        //        var selectionType = SelectionType.none
+        //
+        //        if combineArray.contains(strDate){
+        //            if (combineArray.contains(previousDate) && combineArray.contains(nextDate)) {
+        //                selectionType = .middle
+        //            } else if combineArray.contains(previousDate) && combineArray.contains(strDate) {
+        //                selectionType = .rightBorder
+        //            } else if combineArray.contains(nextDate) {
+        //                selectionType = .leftBorder
+        //            } else {
+        //                selectionType = .single
+        //            }
+        //        }
+        //        if selectionType == .none {
+        //            calCell.eventLayer!.isHidden = true
+        //            return
+        //        }
+        //        calCell.eventLayer!.isHidden = false
+        //        calCell.selectionType = selectionType
     }
     
     //    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
@@ -151,16 +217,16 @@ extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegateAppear
         return [.green, .purple]
     }
     
-    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        // 짝수의 눌림때마다 2번눌러야 풀림 -> 해결해야함
-        if firstDate == nil {
-            firstDate = date
-        } else {
-            calendar.deselect(firstDate!)
-            firstDate = nil
-        }
-        self.configureVisibleCells()
-    }
+    //    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+    //        // 짝수의 눌림때마다 2번눌러야 풀림 -> 해결해야함
+    //        if firstDate == nil {
+    //            firstDate = date
+    //        } else {
+    //            calendar.deselect(firstDate!)
+    //            firstDate = nil
+    //        }
+    //        self.configureVisibleCells()
+    //    }
     
     func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
         let cell = calendar.dequeueReusableCell(withIdentifier: "cell", for: date, at: position)
@@ -170,18 +236,12 @@ extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegateAppear
     func calendar(_ calendar: FSCalendar, willDisplay cell: FSCalendarCell, for date: Date, at position: FSCalendarMonthPosition) {
         cell.eventIndicator.transform = CGAffineTransform(scaleX: cell.frame.width / 25, y: cell.frame.width / 25)
         calendar.appearance.eventOffset = CGPoint(x: cell.frame.width / 3.5, y: -cell.frame.height + cell.frame.height / 18.5)
-        self.configure(cell: cell, for: date, at: position)
+        //        self.configure(cell: cell, for: date, at: position)
     }
+    
+    
+    
+    
+    
 }
 
-//func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-/// 어떻게 쓸지 고민
-//    cell.layer.masksToBounds = true
-//    if self.count == 2 {
-//        cell.layer.cornerRadius = 17
-//        cell.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-//    } else if indexPath.row == 1 {
-//        cell.layer.cornerRadius = 17
-//        cell.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-//    }
-//}
