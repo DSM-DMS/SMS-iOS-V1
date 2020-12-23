9ba9c2befe35230f3db70e854ad9b06c81156e8b
이현욱 <hjhsha827@gmail.com>
2020-12-24 01:55:00 +0900

    [Modify] OutGoingAlertViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingAlertViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingAlertViewController.swift
@@ -8,19 +8,65 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 class OutGoingAlertViewController: UIViewController {
+    let disposeBag = DisposeBag()
     
     @IBOutlet weak var backgroundView: CustomShadowButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    @IBOutlet weak var applicationBtn: UIButton!
     
     let viewModel = OutGoingAlertViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func cancelButton(_ sender: Any) {
-//        viewModel.dismissViewController()
+}
+
+extension OutGoingAlertViewController {
+    func bindAction() {
+        cancelBtn.rx.tap
+            .bind { }
+            .disposed(by: disposeBag)
+        
+        applicationBtn.rx.tap
+            .bind {}
+            .disposed(by: disposeBag)
     }
-    @IBAction func applyButton(_ sender: Any) {
-//        viewModel.presentViewController()
+}
+
+class OutGoingLocationAlertViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var locationSearchBar: UISearchBar!
+    @IBOutlet weak var cancelBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindAction()
+    }
+}
+
+extension OutGoingLocationAlertViewController {
+    func bindAction() {
+        cancelBtn.rx.tap
+            .bind {}
+            .disposed(by: disposeBag)
+    }
+    
+    func bind() {
+        locationSearchBar.rx.text
+            .map { $0 ?? "" }
+            .flatMap { txt -> Observable<OutLocationModel> in
+                SMSAPIClient.shared.networking(from: .location(txt!))
+            }.map { model in
+                model.item
+            }.bind(to: tableView.rx.items(cellIdentifier: LocationTableViewCell.NibName, cellType: LocationTableViewCell.self)) { _, address, cell in
+                cell.addressLbl.text = address.address
+                cell.roadAddressLbl.text = address.roadAddress
+            }.disposed(by: disposeBag)
     }
 }
