b043a3729a465f102e040b29c3403ff0d405cf9f
이현욱 <hjhsha827@gmail.com>
2020-12-23 15:16:19 +0900

    [Modify] TimeScheduleXib

--- SMS_iOS_V1/SMS/SMS/View/Schedule/TimeScheduleXib.swift
+++ SMS_iOS_V1/SMS/SMS/View/Schedule/TimeScheduleXib.swift
@@ -1,9 +1,14 @@
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 class TimeScheduleXib: UIView {
-    private let xibName = "TimeScheduleXib"
+    let disposeBag = DisposeBag()
+    let calendar = Calendar.current
+    lazy var mondayCompo = calendar.dateComponents([.year, .month, .day], from: getMonday(myDate: Date()))
     
-    @IBOutlet weak var dateView: UIView!
+    @IBOutlet weak var dateView: CustomShadowView!
     @IBOutlet var mondayLabels: [UILabel]!
     @IBOutlet var tuesdayLabels: [UILabel]!
     @IBOutlet var wedsLabels: [UILabel]!
@@ -21,13 +26,20 @@ class TimeScheduleXib: UIView {
     }
     
     private func commonInit(){
-        let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
+        let view = Bundle.main.loadNibNamed(TimeScheduleXib.NibName, owner: self, options: nil)?.first as! UIView
         view.frame = self.bounds
         self.addSubview(view)
-        self.dateView.addShadow(offset: CGSize(width: 0, height: 2),
-                           color: .lightGray,
-                           shadowRadius: 2,
-                           opacity: 0.7,
-                           cornerRadius: 5)
+        
+        let monday: Observable<TimeTableModel> = SMSAPIClient.shared.networking(from: .timetables(mondayCompo.year!, mondayCompo.month!, mondayCompo.day!))
+        
+        monday.bind{ model in
+            let arr = [model.time1, model.time2, model.time3, model.time4, model.time5, model.time6, model.time7]
+            for i in 0...6 {
+                self.mondayLabels[i].text = arr[i]
+            }
+        }.disposed(by: disposeBag)
+        
+        
+        
     }
 }
