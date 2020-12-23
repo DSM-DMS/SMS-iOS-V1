7248aeb76531e9538d63043adcdfd31c57d83a49
이현욱 <hjhsha827@gmail.com>
2020-12-23 23:24:34 +0900

    [Modify] Code

--- SMS_iOS_V1/SMS/SMS/View/TableViewCell/OutGoingLogTableViewCell.swift
+++ SMS_iOS_V1/SMS/SMS/View/TableViewCell/OutGoingLogTableViewCell.swift
@@ -8,31 +8,31 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 class OutGoingLogTableViewCell: UITableViewCell {
+    let disposeBag = DisposeBag()
+    
     @IBOutlet weak var statusColorView: UIView!
     @IBOutlet weak var endTimeLbl: UILabel!
     @IBOutlet weak var startTimeLbl: UILabel!
     @IBOutlet weak var outGoingStateLbl: UILabel!
     @IBOutlet weak var placeLbl: UILabel!
     @IBOutlet weak var reasonLbl: UILabel!
-    @IBOutlet weak var detailOutGoing: UIButton!
     @IBOutlet weak var dateLbl: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
         self.addShadow(offset: CGSize(width: 0, height: 2),
                        color: .lightGray,
                        shadowRadius: 2,
                        opacity: 0.7,
                        cornerRadius: 10)
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
 }
