558cc2abf0b43829988bb84664525bbf8cf7f512
이현욱 <hjhsha827@gmail.com>
2020-12-24 01:54:33 +0900

    [Modify] OutGoingPopDeedViewController

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingPopDeedViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingPopDeedViewController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class OutGoingPopDeedViewController: UIViewController {
+import RxCocoa
+import RxSwift
+
+class OutGoingPopDeedViewController: UIViewController, Storyboarded {
+    let disposeBag = DisposeBag()
+    var data: CardData?
+    weak var coordinator: OutGoingCoordinator?
     
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var studentIDLbl: UILabel!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var popAlertBtn: UIButton!
     @IBOutlet weak var backgroundView: CustomShadowView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bind()
     }
+}
+
+extension OutGoingPopDeedViewController {
+    func bind() {
+        popAlertBtn.rx.tap
+            .bind { self.coordinator?.pop()}
+            .disposed(by: disposeBag)
+        
+//        dateLbl.text = data?.date.year
+        nameLbl.text = data?.name
+        studentIDLbl.text = data?.studentID
+    }
+}
+
+
+struct CardData {
+    let studentID: String
+    let name: String
+    let date: DateComponents
 }
