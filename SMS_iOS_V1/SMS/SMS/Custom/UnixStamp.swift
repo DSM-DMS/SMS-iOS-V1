80d0e83bd39cb163664f4a76b6133b51db4776b2
이현욱 <hjhsha827@gmail.com>
2020-12-28 08:23:37 +0900

    [Modify] UnixStamp

--- SMS_iOS_V1/SMS/SMS/Custom/UnixStamp.swift
+++ SMS_iOS_V1/SMS/SMS/Custom/UnixStamp.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
-func UnixStampToDate(with unixInt: Int) -> String {
-    return globalDateFormatter(formType(rawValue: formType.time.rawValue)!).string(from: Date(timeIntervalSince1970: TimeInterval(unixInt)))
+func unix(with unixInt: Int) -> DateComponents {
+    let date = Date(timeIntervalSince1970: TimeInterval(unixInt - 32400))
+    return Calendar.current.dateComponents([.year, .month , .day, .hour, .minute], from: date)
 }
 
-func DateToUnixStamp(with date: String) -> Int {
-    return Int(globalDateFormatter(formType(rawValue: formType.untilDay.rawValue)!).date(from: date)!.timeIntervalSince1970)
+func unix(with dateStr: String) -> Int {
+    let date = DateFormatter().date(from: dateStr)!
+    return Int(date.timeIntervalSince1970)
+}
+
+func stringToUnix(with time: String) -> Int { 
+    let end = time.index(time.endIndex, offsetBy: -4)
+    let timeArr = time[time.startIndex...end]
+    
+    let components = timeArr.split { $0 == ":" } .map { (x) -> Int in return Int(String(x))! }
+    return components[0] * 3600 + components[1] * 60
 }
