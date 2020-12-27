ef39551813143ddc3b7b3b30e753c5603ed186ab
이현욱 <hjhsha827@gmail.com>
2020-12-28 08:24:25 +0900

    [Modify] OutGoingCoordinator

--- SMS_iOS_V1/SMS/SMS/View/Coordinator/OutGoingCoordinator.swift
+++ SMS_iOS_V1/SMS/SMS/View/Coordinator/OutGoingCoordinator.swift
@@ -35,24 +35,30 @@ class OutGoingCoordinator: Coordinator {
     func outGoingApply() {
         let vc = OutGoingApplyViewController.instantiate(storyboardName: .outGoingApply)
         vc.coordinator = self
-        nav.pushViewController(vc, animated: false)
+        nav.pushViewController(vc, animated: true)
     }
     
     func outGoingLog() {
         let vc = OutGoingLogViewController.instantiate(storyboardName: .outGoingLog)
         vc.coordinator = self
-        nav.pushViewController(vc, animated: false)
+        nav.pushViewController(vc, animated: true)
     }
     
     func noticeOutGoing() {
         let vc = OutGoingNoticeViewController.instantiate(storyboardName: .outGoingNotice)
         vc.coordinator = self
-        nav.pushViewController(vc, animated: false)
+        nav.pushViewController(vc, animated: true)
     }
     
-    func deedOutGoing() {
-        let vc = OutGoingDeedViewController.instantiate(storyboardName: .outGoingDeed)
+    func outGoingCompleted() {
+        let vc = OutGoingCompletedViewController.instantiate(storyboardName: .outGoingCompleted)
         vc.coordinator = self
-        nav.pushViewController(vc, animated: false)
+        nav.pushViewController(vc, animated: true)
+    }
+    
+    func popUp() {
+        let vc = OutGoingPopDeedViewController.instantiate(storyboardName: .outGoingPopUp)
+        vc.coordinator = self
+        nav.pushViewController(vc, animated: true)
     }
 }
