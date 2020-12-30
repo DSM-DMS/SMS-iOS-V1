1ea56a1897bb65276d47c04d150b479e7f0f5829
이현욱 <hjhsha827@gmail.com>
2020-12-30 15:18:36 +0900

    [Modify] code

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingCompletedViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingCompletedViewController.swift
@@ -27,8 +27,13 @@ extension OutGoingCompletedViewController {
     private func bindAction() {
         checkButton.rx.tap
             .bind { _ in
-                let controllers = self.coordinator?.nav.viewControllers
-                self.coordinator?.nav.popToViewController(controllers![controllers!.count - 2], animated: true)
+                guard var controllers = self.coordinator?.nav.viewControllers else { return }
+                let count = controllers.count
+                if count > 2 {
+                    controllers.removeSubrange(1...count-2)
+                }
+                self.coordinator?.nav.viewControllers = controllers
+                self.coordinator?.nav.popViewController(animated: true)
             }
             .disposed(by: disposeBag)
     }
