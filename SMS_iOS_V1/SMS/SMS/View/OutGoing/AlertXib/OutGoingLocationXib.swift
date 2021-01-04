c6f0ff10bfbe3368a5efcceffef0d70ba3d79e7d
이현욱 <hjhsha827@gmail.com>
2021-01-04 09:27:45 +0900

    [Add] OutGoingLocationXib

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/AlertXib/OutGoingLocationXib.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/AlertXib/OutGoingLocationXib.swift
@@ -1,5 +1,61 @@
+////
+////  OutGoingLocationXib.swift
+////  SMS
+////
+////  Created by 이현욱 on 2020/12/30.
+////  Copyright © 2020 DohyunKim. All rights reserved.
+////
 //
-//  OutGoingLocationXib.swift
+//import UIKit
+//
+//import RxSwift
+//import RxCocoa
+//
+//class OutGoingLocationAlertView: UIView {
+//    let disposeBag = DisposeBag()
+////    var sign: ((String) -> ())?
+//
+//    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var locationSearchBar: UISearchBar!
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        self.commonInit()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        self.commonInit()
+//    }
+//
+//    private func commonInit(){
+//        let view = Bundle.main.loadNibNamed(OutGoingLocationAlertView.NibName, owner: self, options: nil)?.first as! UIView
+//        view.frame = self.bounds
+//        self.addSubview(view)
+////        tableView.rowHeight = 70
+//    }
+//}
+//
+//extension OutGoingLocationAlertView {
+//    func bind() {
+//        locationSearchBar.rx.searchButtonClicked
+//            .map { self.locationSearchBar.text! }
+//            .filter { txt in
+//                txt != "" || !txt.isEmpty
+//            }
+//            .distinctUntilChanged()
+//            .flatMap { txt -> Observable<OutLocationModel> in
+//                SMSAPIClient.shared.networking(from: .location(txt))
+//            }.map { model in
+//                model.item!
+//            }.bind(to: tableView.rx.items(cellIdentifier: LocationTableViewCell.NibName, cellType: LocationTableViewCell.self)) { _, address, cell in
+//                cell.addressLbl.text = address.address
+//                cell.roadAddressLbl.text = address.roadAddress
+//            }.disposed(by: disposeBag)
+//    }
+//}
+//
+//  view.swift
 //  SMS
 //
 //  Created by 이현욱 on 2020/12/30.
@@ -13,13 +69,10 @@ import RxCocoa
 
 class OutGoingLocationAlertView: UIView {
     let disposeBag = DisposeBag()
-    var sign: ((String) -> ())? = nil
-    weak var coordinator: OutGoingCoordinator?
+    var sign: ((Bool) -> ())? = nil
     
-    @IBOutlet weak var tableView2: UITableView!
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var locationSearchBar: UISearchBar!
-    @IBOutlet weak var cancelBtn: UIButton!
+//    @IBOutlet weak var cancelBtn: UIButton!
+//    @IBOutlet weak var applicationBtn: UIButton!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,42 +82,33 @@ class OutGoingLocationAlertView: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.commonInit()
+//        bindAction()
     }
     
     private func commonInit(){
-        let view = Bundle.main.loadNibNamed(OutGoingAlertXib.NibName, owner: self, options: nil)?.first as! UIView
+        let view = Bundle.main.loadNibNamed(OutGoingLocationAlertView.NibName, owner: self, options: nil)?.first as! UIView
         view.frame = self.bounds
         self.addSubview(view)
-        bindAction()
-        bind()
-    }
-}
-
-extension OutGoingLocationAlertView {
-    func bindAction() {
-        cancelBtn.rx.tap
-            .bind { self.coordinator?.pop() }
-            .disposed(by: disposeBag)
     }
     
-    func bind() {
-        tableView.rowHeight = 70
-        locationSearchBar.rx.searchButtonClicked
-            .map { self.locationSearchBar.text! }
-            .distinctUntilChanged()
-            .flatMap { txt -> Observable<OutLocationModel> in
-                SMSAPIClient.shared.networking(from: .location(txt))
-            }.map { model in
-                model.item!
-            }.bind(to: tableView.rx.items(cellIdentifier: LocationTableViewCell.NibName, cellType: LocationTableViewCell.self)) { _, address, cell in
-                cell.addressLbl.text = address.address
-                cell.roadAddressLbl.text = address.roadAddress
-            }.disposed(by: disposeBag)
-        
-        Observable.zip(self.tableView.rx.itemSelected, self.tableView.rx.modelSelected(itmesArr.self))
-            .bind { [unowned self] _, model in
-                sign?(model.address)
-                self.coordinator?.pop()
-            }.disposed(by: self.disposeBag)
-    }
+//    @IBAction func cancelTapped(_ sender: UIButton) {
+//        sign?(false)
+//    }
+//    @IBAction func OKTapped(_ sender: UIButton) {
+//        sign?(true)
+//    }
 }
+
+//extension OutGoingAlertXib {
+//    func bindAction() {
+//        cancelBtn.rx.tap
+//            .bind { self.sign?(false) }
+//            .disposed(by: disposeBag)
+//        
+//        applicationBtn.rx.tap
+//            .bind { _ in
+//                self.sign?(true)
+//            }
+//            .disposed(by: disposeBag)
+//    }
+//}
