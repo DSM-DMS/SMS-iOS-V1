18be575705f96706b86b94c1beab59082648ebd0
DohyunKIm <rlaehgus1229@gmail.com>
2021-01-06 20:18:38 +0900

    [Add] MypageViewModel

---
+++ SMS_iOS_V1/SMS/SMS/ViewModel/Mypage/MypageViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MypageViewModel.swift
+//  SMS
+//
+//  Created by DohyunKim on 2021/01/06.
+//  Copyright © 2021 DohyunKim. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+class MypageViewModel {
+    
+}
