3fba10edcfd6081eb0ee4c7ee4259163c6603eba
DohyunKIm <rlaehgus1229@gmail.com>
2021-01-15 22:57:13 +0900

    [Add] MypageModel

---
+++ SMS_iOS_V1/SMS/SMS/Model/Mypage/MypageModel.swift
@@ -0,0 +1,21 @@
+//
+//  MypageModel.swift
+//  SMS
+//
+//  Created by DohyunKim on 2021/01/15.
+//  Copyright © 2021 DohyunKim. All rights reserved.
+//
+
+import Foundation
+
+struct MypageModel {
+    let status: Int
+    let code: Int
+    let message: String
+    let grage: Int
+    let group: Int
+    let studentNumber: Int
+    let name: String
+    let phone_number: String
+    let profileUri: String
+}
