f0d4f6a9913c0d6b354e9075406444d22967b487
DohyunKIm <rlaehgus1229@gmail.com>
2021-01-19 23:37:18 +0900

    [Modify] mypageViewController

--- SMS_iOS_V1/SMS/SMS/View/MyPage/MypageViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/MyPage/MypageViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MypageViewController: UIViewController, Storyboarded {
     weak var coordinator: MyPageCoordinator?
+    
+    let viewModel = MypageViewModel()
+    let disposeBag = DisposeBag()
     
     @IBOutlet weak var pwChangeButton: CustomShadowButton!
     @IBOutlet weak var logOutButton: CustomShadowButton!
     @IBOutlet weak var introduceDevButton: CustomShadowButton!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func bind() {
+        
+        viewModel.mypageData.subscribe { model in
+            
+        }
+        
     }
     
 }
