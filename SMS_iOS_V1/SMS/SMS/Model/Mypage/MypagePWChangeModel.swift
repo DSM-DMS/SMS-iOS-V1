446b66d10d4c9bd8319c86319df9cb2d4be78b8e
DohyunKIm <rlaehgus1229@gmail.com>
2021-01-21 20:48:43 +0900

    [Add] MypagePWChangeModel

---
+++ SMS_iOS_V1/SMS/SMS/Model/Mypage/MypagePWChangeModel.swift
@@ -0,0 +1,16 @@
+//
+//  MypagePWChangeModel.swift
+//  SMS
+//
+//  Created by DohyunKim on 2021/01/21.
+//  Copyright © 2021 DohyunKim. All rights reserved.
+//
+
+import Foundation
+
+struct mypagePWChangeModel: Codable {
+    
+    let current_pw: String
+    let revision_pw: String
+    
+}
