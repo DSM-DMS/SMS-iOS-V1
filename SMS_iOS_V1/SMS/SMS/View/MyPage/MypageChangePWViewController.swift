6bf7e701bc527bead2ecd63a9ae05e61c87e3395
DohyunKIm <rlaehgus1229@gmail.com>
2021-01-27 21:09:39 +0900

    [Modify] MypagePWViewController

--- SMS_iOS_V1/SMS/SMS/View/MyPage/MypageChangePWViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/MyPage/MypageChangePWViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import RxSwift
 
 class MypageChangePWViewController: UIViewController, Storyboarded {
     weak var coordinator: MyPageCoordinator?
     
+    let viewModel = MypageChangePWViewModel()
+    let disposeBag = DisposeBag()
+    
+    
     @IBOutlet weak var applyButton: CustomShadowButton!
+    @IBOutlet weak var currentPWTextField: UITextField!
+    @IBOutlet weak var newPWTextField: UITextField!
+    @IBOutlet weak var confirmPWTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+extension MypageChangePWViewController {
+    func bind() {
+        
+        
+    }
+    
 }
