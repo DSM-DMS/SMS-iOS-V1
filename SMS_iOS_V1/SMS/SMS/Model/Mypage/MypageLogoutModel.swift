ab766e5846314daead060bc331770017a5740d75
DohyunKIm <rlaehgus1229@gmail.com>
2021-02-02 16:43:14 +0900

    [Add] LogoutModel, ViewModel

---
+++ SMS_iOS_V1/SMS/SMS/Model/Mypage/MypageLogoutModel.swift
@@ -0,0 +1,14 @@
+//
+//  MypageLogoutModel.swift
+//  SMS
+//
+//  Created by DohyunKim on 2021/02/02.
+//  Copyright © 2021 DohyunKim. All rights reserved.
+//
+
+import Foundation
+
+struct MypageLogoutModel: Codable {
+    
+    
+}
---
+++ SMS_iOS_V1/SMS/SMS/ViewModel/Mypage/MypageLogoutViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MypageLogoutViewModel.swift
+//  SMS
+//
+//  Created by DohyunKim on 2021/02/02.
+//  Copyright © 2021 DohyunKim. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import KeychainSwift
+
+class MypageLogoutViewModel {
+    
+}
