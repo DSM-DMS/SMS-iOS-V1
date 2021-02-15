49031e6b80344dc18c6887099cbf60b35ab7cda5
이현욱 <hjhsha827@gmail.com>
2021-02-16 02:18:24 +0900

    [Modify]

--- SMS_iOS_V1/SMS/SMS/View/CollectionViewCell/CustomCell.swift
+++ SMS_iOS_V1/SMS/SMS/View/CollectionViewCell/CustomCell.swift
@@ -2,13 +2,11 @@ import UIKit
 
 class CustomCell: UICollectionViewCell {
     
-    static let xibName = "CustomCell"
-    
     @IBOutlet var imageView: UIImageView!
     
     var tabbar:Tabbar? {
         didSet {
-            guard let tabbar:Tabbar = tabbar else { return }
+            guard let tabbar: Tabbar = tabbar else { return }
             self.imageView.image = UIImage(named: tabbar.imageName)?.withRenderingMode(.alwaysTemplate)
             self.imageView.tintColor = tabbar.isSelected ? .customPurple : .customBlack
         }
