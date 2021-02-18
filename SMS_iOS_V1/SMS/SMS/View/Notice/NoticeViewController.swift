319a01c29834fd2bb486292af6ce131b6fa11cb0
DohyunKIm <rlaehgus1229@gmail.com>
2021-02-18 17:46:37 +0900

    [Modify] NoticeViewController bind logic

--- SMS_iOS_V1/SMS/SMS/View/Notice/NoticeViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/Notice/NoticeViewController.swift
@@ -31,16 +31,33 @@ class NoticeViewController: UIViewController, Storyboarded {
 }
 extension NoticeViewController {
     
-    func bind() {
-        viewModel.noticeData.subscribe { data in
-            data.element?.
-//            if data.element?.status == 200 || data.element?.code == 200 {
-//
+    func UIbind() {
+        viewModel.noticeData.subscribe { [self] data in
+            
+            let idx = data.element?.number
+            let title = data.element?.title
+            let date = data.element?.date
+            let views = data.element?.views
+            let cellElements = [idx!, title!, date!, views!] as [Any]
+            if data.element?.status == 200 || data.element?.code == 200 {
+                Observable.from(optional: cellElements)
+                    .bind(to: self.noticeTableView.rx.items(cellIdentifier: NoticeTableViewCell.NibName)) {_, cellElements, cell in
+                        if let celltoUse = cell as? NoticeTableViewCell {
+                            celltoUse.cellNumber.text = cellElements[0]
+                            celltoUse.cellTitle.text = cellElements[1]
+                            celltoUse.cellDate.text = cellElements[2]
+                            celltoUse.cellViews.text = cellElements[3]
+                        }
+                        
+                    }
+                
 //                noticeTableView.rx.items(cellIdentifier: NoticeTableViewCell.NibName)
-//
-//
-//            }
+                
+                
+            }
             
         }.disposed(by: disposeBag)
+        
     }
+    
 }
