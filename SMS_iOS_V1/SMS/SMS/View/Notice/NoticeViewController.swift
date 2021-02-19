ec5ade61028ab4d9b1f9f0cd021486fef199e068
이현욱 <hjhsha827@gmail.com>
2021-02-19 19:09:04 +0900

    [Modify] Notice

--- SMS_iOS_V1/SMS/SMS/View/Notice/NoticeViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/Notice/NoticeViewController.swift
@@ -12,7 +12,6 @@ import RxCocoa
 
 class NoticeViewController: UIViewController, Storyboarded {
     weak var coordinator: NoticeCoordinator?
-    
     let viewModel = NoticeViewModel()
     let disposeBag = DisposeBag()
     
@@ -21,43 +20,31 @@ class NoticeViewController: UIViewController, Storyboarded {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIbind()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         coordinator?.disappear()
     }
-    
 }
+
+
+
 extension NoticeViewController {
-    
     func UIbind() {
-        viewModel.noticeData.subscribe { [self] data in
-            
-            let idx = data.element?.number
-            let title = data.element?.title
-            let date = data.element?.date
-            let views = data.element?.views
-            let cellElements = [idx!, title!, date!, views!] as [Any]
-            if data.element?.status == 200 || data.element?.code == 200 {
-                Observable.from(optional: cellElements)
-                    .bind(to: self.noticeTableView.rx.items(cellIdentifier: NoticeTableViewCell.NibName)) {_, cellElements, cell in
-                        if let celltoUse = cell as? NoticeTableViewCell {
-                            celltoUse.cellNumber.text = cellElements[0]
-                            celltoUse.cellTitle.text = cellElements[1]
-                            celltoUse.cellDate.text = cellElements[2]
-                            celltoUse.cellViews.text = cellElements[3]
-                        }
-                        
-                    }
-                
-//                noticeTableView.rx.items(cellIdentifier: NoticeTableViewCell.NibName)
-                
-                
-            }
-            
-        }.disposed(by: disposeBag)
+        let Notice: Observable<NoticeModel> = SMSAPIClient.shared.networking(from: .lookUpNotice)
         
+        Notice.subscribe(onNext: { model in
+            if model.status == 200 {
+                Observable.of(model.announcements ?? [])
+                    .bind(to: self.noticeTableView.rx.items(cellIdentifier: NoticeTableViewCell.NibName, cellType: NoticeTableViewCell.self)) { idx, notice, cell in
+                        cell.cellDate.text = unix(with: notice.date)
+                        cell.cellNumber.text = "\(notice.number)"
+                        cell.cellTitle.text = notice.title
+                        cell.cellViews.text = "\(notice.views)"
+                    }.disposed(by: self.disposeBag)
+            }
+        }).disposed(by: disposeBag)
     }
-    
 }
