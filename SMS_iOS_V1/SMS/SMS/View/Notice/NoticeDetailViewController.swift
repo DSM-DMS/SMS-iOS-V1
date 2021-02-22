3e27139bf4794e531012a2c108d732871441db33
DohyunKIm <rlaehgus1229@gmail.com>
2021-02-22 14:31:04 +0900

    [Add] func bind

--- SMS_iOS_V1/SMS/SMS/View/Notice/NoticeDetailViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/Notice/NoticeDetailViewController.swift
@@ -24,15 +24,35 @@ class NoticeDetailViewController: UIViewController, Storyboarded {
     @IBOutlet weak var previousNotice: UIButton!
     @IBOutlet weak var nextNotice: UIButton!
     
+    //previous noticebtn
+    //nextnoticebtn
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
     }
 }
 
 extension NoticeDetailViewController {
     func bind() {
-        
+        viewModel.NoticeDetailData.subscribe { [self] data in
+            let model = try? NoticeDetailModel(from: data as! Decoder)
+            if (Int(model!.status) == 200) {
+                noticeTitle.text = model?.title
+                date.text = String(model!.date)
+//                views.text = String(model!.views) API에 조회수 추가되면 NoticeDetailModel에 views 추가하고 주석 풀 것.
+                notice.text = model?.content
+                previousNotice.setTitle(model?.previous_title, for: .normal)
+                nextNotice.setTitle(model?.next_title, for: .normal)
+            }
+            else {
+                print("404 error")
+                fatalError()
+            }
+            
+        }.disposed(by: disposeBag)
         
     }
+    
 }
