4f8dce7ceaba3a06495781be2a2bf6a07d47cce6
이현욱 <hjhsha827@gmail.com>
2021-02-26 14:26:51 +0900

    [Modify] Login

--- SMS_iOS_V1/SMS/SMS/View/Auth/LoginViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/Auth/LoginViewController.swift
@@ -10,12 +10,11 @@ import UIKit
 
 import RxSwift
 import RxCocoa
-import KeychainSwift
 import SkyFloatingLabelTextField
 
-class LoginViewController: UIViewController, Storyboarded {
+final class LoginViewController: UIViewController, Storyboarded {
     weak var coordinator: AppCoordinator?
-    let viewModel = LoginViewModel()
+    private let viewModel = LoginViewModel()
     let disposeBag = DisposeBag()
     
     @IBOutlet weak var registerButton: UIButton!
@@ -27,27 +26,34 @@ class LoginViewController: UIViewController, Storyboarded {
     override func viewDidLoad() {
         super.viewDidLoad()
         bind()
+        bindAction()
+        loginButton.addShadow(offset: CGSize(width: 0, height: 3),
+                              color: .gray,
+                              shadowRadius: 6,
+                              opacity: 1,
+                              cornerRadius: 5)
     }
 }
 
 extension LoginViewController {
     func bind() {
-        let input = LoginViewModel.Input.init(idTextFieldDriver: idTextField.rx.text.orEmpty.asDriver(), pwTextFieldDriver: pwTextField.rx.text.orEmpty.asDriver(), loginBtnDriver: loginButton.rx.tap.asDriver(), autoLoginDriver: autoLoginCheckBox.rx.tap.asDriver())
+        let input = LoginViewModel.Input(idTextFieldDriver: idTextField.rx.text.orEmpty.asDriver(), pwTextFieldDriver:  pwTextField.rx.text.orEmpty.asDriver(), loginBtnDriver: loginButton.rx.tap.asDriver(), autoLoginDriver: autoLoginCheckBox.rx.tap.asDriver())
+        
+        let isValid = viewModel.isValid(input)
+        
+        isValid.bind(to: self.loginButton.rx.isEnabled).disposed(by: disposeBag)
+        isValid.map { $0 ? 1 : 0.3 }.bind(to: self.loginButton.rx.alpha).disposed(by: disposeBag)
         
         let output = viewModel.transform(input)
         
         output.result.subscribe { model in
             if model.status == 200 || model.code == 200 {
-                let keyChain = KeychainSwift()
-                
                 self.coordinator?.tabbar()
                 if self.autoLoginCheckBox.isSelected {
                     keyChain.set(self.idTextField.text!, forKey: "ID")
-                    print("keychian saved")
                     keyChain.set(self.pwTextField.text!, forKey: "PW")
-                } else { 
+                } else {
                     keyChain.delete("ID")
-                    print("keychian deleted")
                     keyChain.delete("PW")
                 }
             } else {
@@ -61,7 +67,7 @@ extension LoginViewController {
     func bindAction() {
         registerButton.rx.tap
             .bind { _ in
-                // 코디네이터 추가하면 하기 
+                self.coordinator?.checkNumber()
             }.disposed(by: disposeBag)
     }
 }
