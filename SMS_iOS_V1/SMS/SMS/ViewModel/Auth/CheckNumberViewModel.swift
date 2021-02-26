a45303fc15b073bbfab20d3a250588ec114f7442
이현욱 <hjhsha827@gmail.com>
2021-02-26 14:26:22 +0900

    [Modify] CheckNumberViewModel

--- SMS_iOS_V1/SMS/SMS/ViewModel/Auth/CheckNumberViewModel.swift
+++ SMS_iOS_V1/SMS/SMS/ViewModel/Auth/CheckNumberViewModel.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+import RxSwift
+import RxCocoa
+
+class CheckNumberViewModel: ViewModelType {
+    struct Input {
+        let numberDriver: Driver<String>
+        let checkDrvier: Driver<Void>
+    }
+    
+    struct Output {
+        let certificationNumberModel: Observable<CertificationNumberModel>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let numberTextField = input.numberDriver.asObservable()
+        
+        let model = input.checkDrvier.asObservable()
+            .withLatestFrom(numberTextField)
+            .flatMap { number -> Observable<CertificationNumberModel> in
+                SMSAPIClient.shared.networking(from: .certificationNumber(number))
+            }
+        
+        return Output(certificationNumberModel: model)
+    }
+}
