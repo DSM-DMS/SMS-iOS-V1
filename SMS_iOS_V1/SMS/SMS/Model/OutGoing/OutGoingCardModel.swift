2223e14a1978b6433ba14e7158d9b1974c53a6f0
이현욱 <hjhsha827@gmail.com>
2021-03-02 23:17:24 +0900

    [Modify] Model

--- SMS_iOS_V1/SMS/SMS/Model/OutGoing/OutGoingCardModel.swift
+++ SMS_iOS_V1/SMS/SMS/Model/OutGoing/OutGoingCardModel.swift
@@ -13,14 +13,14 @@ struct OutGoingCardModel: Codable {
     let status: Int
     let code: Int
     let message: String
-    let outing_uuid: String
-    let place: String
-    let start_time: Int
-    let end_time: Int
-    let outing_status: String
-    let name: String
-    let grade: Int
-    let group: Int
-    let number: Int
-    let profile_uri: String
+    let outing_uuid: String?
+    let place: String?
+    let start_time: Int?
+    let end_time: Int?
+    let outing_status: String?
+    let name: String?
+    let grade: Int?
+    let group: Int?
+    let number: Int?
+    let profile_uri: String?
 }
---
+++ SMS_iOS_V1/SMS/SMS/Model/OutGoing/OutingActionModel.swift
@@ -0,0 +1,15 @@
+//
+//  OutingActionModel.swift
+//  SMS
+//
+//  Created by 이현욱 on 2021/03/02.
+//  Copyright © 2021 DohyunKim. All rights reserved.
+//
+
+import Foundation
+
+struct OutingActionModel: Codable {
+    let status: Int
+    let code: Int
+    let message: String
+}
