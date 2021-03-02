4c2ae65295ae419af9a711ac7edd2d49b962a9de
이현욱 <hjhsha827@gmail.com>
2021-03-02 23:14:04 +0900

    [Modify] LoginVIewModel

--- SMS_iOS_V1/SMS/SMS/ViewModel/Login/LoginViewModel.swift
+++ SMS_iOS_V1/SMS/SMS/ViewModel/Login/LoginViewModel.swift
@@ -27,14 +27,15 @@ class LoginViewModel {
     
     func transform(_ input: Input) -> Output {
         let loginResponse = input.loginBtnDriver.asObservable()
+            .debounce(.seconds(1), scheduler: ConcurrentDispatchQueueScheduler.init(qos: .background))
             .withLatestFrom(Observable.combineLatest(input.idTextFieldDriver.asObservable(),
                                                      input.pwTextFieldDriver.asObservable()))
-            .filter{ !$0.0.isEmpty && !$0.1.isEmpty}
+            .filter{ !$0.0.isEmpty && !$0.1.isEmpty }
             .map { (id, pw) -> SMSAPI in
                 return SMSAPI.login(id, pw)
             }.flatMap { request -> Observable<LoginModel> in
                 SMSAPIClient.shared.networking(from: request)
-            }
+            }.observeOn(MainScheduler.instance)
         return Output(result: loginResponse)
     }
 }
