af5145427f0b9eca962be96b1d3aac0b84944e4f
이현욱 <hjhsha827@gmail.com>
2021-03-09 07:30:36 +0900

    [Modify] OutGoing

--- SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingViewController.swift
+++ SMS_iOS_V1/SMS/SMS/View/OutGoing/OutGoingViewController.swift
@@ -37,22 +37,6 @@ extension OutGoingViewController {
                 self.coordinator?.outGoingApply();
             }.disposed(by: disposeBag)
         
-<<<<<<< HEAD
-        outGoingLogButton.rx.tap
-            .bind {
-                self.coordinator?.outGoingLog()
-            }.disposed(by: disposeBag)
-        
-        outGoingNoticeButton.rx.tap
-            .bind {
-                self.coordinator?.noticeOutGoing()
-            }.disposed(by: disposeBag)
-        
-        outGoingPopUpBtn.rx.tap
-            .bind {
-                self.coordinator?.outGoingPopDeed()
-            }.disposed(by: disposeBag)
-=======
             outGoingLogButton.rx.tap
             .bind { self.coordinator?.outGoingLog()
             }
@@ -63,11 +47,6 @@ extension OutGoingViewController {
             }
             .disposed(by: disposeBag)
         
-//        outGoingPopUpBtn.rx.tap
-////            .bind { self.coordinator?.()
-//            }
-//            .disposed(by: disposeBag)
->>>>>>> Develop
     }
 }
 
